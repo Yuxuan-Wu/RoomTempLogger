f18f966e3da14758e6242a7a7495ebbc56d1dfc7
Yuxuan-Wu <charleslele2002@gmail.com>
2023-06-29 15:47:40 -0700

    History data view implemented

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+// Load the Visualization API and the corechart package.
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawCharts);
+
 realtimeTemp = document.getElementById('temperature');
 realtimeHumidity = document.getElementById('humidity');
 
@@ -21,7 +25,7 @@ source.onmessage = function (event) {
 
 
 function toggleView() {
-	var currentView = document.getElementById("currentTempView");
+	var currentView = document.getElementById("currentDataView");
 	var historyView = document.getElementById("historyView");
 	if (currentView.style.display === "none") {
 		currentView.style.display = "block";
@@ -29,7 +33,52 @@ function toggleView() {
 	} else {
 		currentView.style.display = "none";
 		historyView.style.display = "block";
+		drawCharts();
 	}
 }
 
-// Chart.js code will go here for the history view
+function drawCharts() {
+	fetch('/history')
+		.then(response => response.json())
+		.then(data => {
+			var tempDataTable = new google.visualization.DataTable();
+			tempDataTable.addColumn('datetime', 'Time');
+			tempDataTable.addColumn('number', 'Temperature (°C)');
+
+			var humidityDataTable = new google.visualization.DataTable();
+			humidityDataTable.addColumn('datetime', 'Time');
+			humidityDataTable.addColumn('number', 'Humidity (%)');
+
+			data.forEach(item => {
+				// Convert timestamp to local time
+				let localTime = new Date(item.timestamp);
+				localTime.setMinutes(localTime.getMinutes() - localTime.getTimezoneOffset());
+
+				tempDataTable.addRow([localTime, item.temperature]);
+				humidityDataTable.addRow([localTime, item.humidity]);
+			});
+
+			// Set chart options
+			var tempOptions = {
+				title: 'Temperature Over Time',
+				curveType: 'function',
+				legend: { position: 'bottom' },
+				height: 400,
+				colors: ['#f1c40f']
+			};
+
+			var humidityOptions = {
+				title: 'Humidity Over Time',
+				curveType: 'function',
+				legend: { position: 'bottom' },
+				height: 400,
+				colors: ['#3498db']
+			};
+
+			var tempChart = new google.visualization.LineChart(document.getElementById('temperatureView'));
+			tempChart.draw(tempDataTable, tempOptions);
+
+			var humidityChart = new google.visualization.LineChart(document.getElementById('humidityView'));
+			humidityChart.draw(humidityDataTable, humidityOptions);
+	});
+}
--- server.js
+++ server.js
@@ -6,12 +6,37 @@ const port = 8080;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+/** Database */
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./sensorData.db', (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log('Connected to the sensorData database.');
+});
+
+db.run('CREATE TABLE IF NOT EXISTS data(id INTEGER PRIMARY KEY AUTOINCREMENT, temperature REAL, humidity REAL, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)', (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+});
+
 /** DHT reader */
 const dht_reader = spawn('python3', ['./dht_reader.py'], { stdio: ['inherit', 'pipe', 'inherit', 'ipc'] });
 let sensorData = { temperature: 'N/A', humidity: 'N/A' };
 
 dht_reader.stdout.on('data', (data) => {
 	sensorData = JSON.parse(data);
+
+	// Insert a row of data into SQLite
+	db.run(`INSERT INTO data(temperature, humidity) VALUES(?, ?)`, [sensorData.temperature, sensorData.humidity], 
+		function (err) {
+			if (err) {
+				return console.log(err.message);
+			}
+			console.log(`A row has been inserted with rowid ${this.lastID}`);
+		}
+	);
 });
 
 /** Routes */
@@ -28,6 +53,29 @@ app.get('/temperature', (req, res) => {
 	}, 1000);
 });
 
+app.get('/history', (req, res) => {
+	let query = `SELECT temperature, humidity, timestamp FROM data ORDER BY timestamp DESC`;
+	db.all(query, [], (err, rows) => {
+		if (err) {
+			res.status(400).send(err.message);
+		}
+		else {
+			res.json(rows);
+		}
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
+});
+
+process.on('SIGINT', () => {
+	console.log('Received SIGINT. Shutting down gracefully...');
+	db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+		console.log('Closed the database connection.');
+	});
+	process.exit(0);
 });
